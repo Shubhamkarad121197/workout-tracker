2510d15d938dc7030f51b471c4d7035db1fc092e
shubham karad <shubham.karad12@gmail.com>
2025-12-02 23:20:50 +0530

    updated workouts

--- src/globalStore.js
+++ src/globalStore.js
@@ -1,21 +1,19 @@
 // globalStore.js
 
+let state = {
+  workouts: []   // global state list
+};
 
-let state={
-    workOuts:[]
-}
-
-let subscribers=[];
+let subscribers = [];
 
 // Read global state
-
-export const getState=()=>state;
+export const getState = () => state;
 
 // Update store and notify
-export const addWorkout=(workout)=>{
-    state={...state,workouts:[...state.workouts,workout]}
-    notifySubscribers()
-}
+export const addWorkout = (workout) => {
+  state = { ...state, workouts: [...state.workouts, workout] };
+  notifySubscribers();
+};
 
 // Subscription — auto re-render UI
 export const subscribe = (callback) => {
